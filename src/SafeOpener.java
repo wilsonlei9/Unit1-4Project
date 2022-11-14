10d39f04409e9548710061c86205401cfe0f9f88
BT_4N2_27 <wilsonl9@nycstudents.net>
2022-11-14 12:33:47 -0500

    Added more to methods

--- src/SafeOpener.java
+++ src/SafeOpener.java
@@ -1,21 +1,45 @@
+import java.util.Scanner;
 public class SafeOpener {
     private int digit1 = (int) (Math.random() * 10);
-    private int digit2 = (int) (Math.random()*10);
-    private int digit3 = (int) (Math.random()*10);
-    private int digit4 = (int) (Math.random()*10);
+    private int digit2 = (int) (Math.random() * 10);
+    private int digit3 = (int) (Math.random() * 10);
+    private int digit4 = (int) (Math.random() * 10);
     private boolean safeOpened;
+    Scanner s = new Scanner(System.in);
 
-    public SafeOpener()
-    {
+    public SafeOpener() {
 
     }
 
-    public String combination()
-    {
+    public String combination() {
         String combination = "" + digit1 + digit2 + digit3 + digit4;
         return combination;
     }
 
+    public void guessDigit1(int guess, int digit1) {
+        this.digit1 = digit1;
+        int count = 0;
+        while (guess != digit1) {
+            if (guess > digit1) {
+                System.out.println("Too high\nTry again");
+                guess = s.nextInt();
+            }
+            if (guess < digit1) {
+                System.out.println("Too low\nTry again");
+                guess = s.nextInt();
+            }
+            if (guess == digit1) {
+                System.out.println("Correct");
+            }
+            count++;
+            if (count == 2 && guess != digit1)
+            {
+                System.out.println("Failed\nThe digit was " + digit1);
+
+            }
+
+        }
+    }
 
 
 }
--- src/SafeOpenerRunner.java
+++ src/SafeOpenerRunner.java
@@ -3,16 +3,44 @@ public class SafeOpenerRunner {
     public static void main(String[] args) {
         SafeOpener safe = new SafeOpener();
         String combination = safe.combination();
-        String digit1 = combination.substring(0,1);
-        String digit2 = combination.substring(1,2);
-        String digit3 = combination.substring(2,3);
-        String digit4 = combination.substring(3,4);
+        int digit1 = Integer.parseInt(combination.substring(0,1));
+        int digit2 = Integer.parseInt(combination.substring(1,2));
+        int digit3 = Integer.parseInt(combination.substring(2,3));
+        int digit4 = Integer.parseInt(combination.substring(3,4));
         Scanner s = new Scanner(System.in);
-        System.out.print(digit1);
+        System.out.println("Try to break into the safe. A digit can not repeat itself in the combination.\nYou have 3 tries to guess each digit");
         System.out.println("Guess the first digit to the combination");
         int guessDigit1 = s.nextInt();
+        int count = 0;
+        safe.guessDigit1(guessDigit1, digit1);
 
 
+        System.out.println("Guess the second digit to the combination");
+        int guessDigit2 = s.nextInt();
+        while (guessDigit2 != digit2)
+        {
+            count = 0;
+            if (guessDigit2 > digit2)
+            {
+                System.out.println("Too high\nTry again");
+                guessDigit2 = s.nextInt();
+            }
+            if (guessDigit2 < digit2)
+            {
+                System.out.println("Too low\nTry again");
+                guessDigit2 = s.nextInt();
+            }
+            if (guessDigit2 == digit2)
+            {
+                System.out.println("Correct");
+            }
+            count++;
+            if (count == 2 && guessDigit1 != digit2)
+            {
+                System.out.println("Failed\nThe digit was " + digit2);
+                return;
+            }
+        }
     }
 }
 
